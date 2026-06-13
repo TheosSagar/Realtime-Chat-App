2146f21397f8c4b3d9a0f1b9f3d7b6aefabbb3f6
TheosSagar <chaurasiyasagar00246@gmail.com>
2026-06-13 22:10:27 +0545

    Made APIs for sending and receiving messages

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const messageRoutes = require('./routes/messageRoutes')
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/messages', messageRoutes)
 
 module.exports = app;
---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,68 @@
+const Message = require('../models/Message')
+
+// Send message
+const sendMessage = async(req, res) => {
+    try{
+        const senderId = req.user._id;
+        const { receiverId } = req.params;
+        const {text} = req.body;
+
+        if(!text) {
+            return res.status(400).json({
+                message: 'Message text is required',
+                
+            })
+        }
+
+        const newMessage = await Message.create({ // Saves the data as new document in collection
+            senderId,
+            receiverId,
+            text,
+        })
+
+        res.status(201).json({
+            message: 'Message sent successfully',
+            newMessage,
+        })
+    }catch(error){
+        res.status(500).json({
+            message: 'Server error',
+        })
+    }
+
+}
+
+// get message
+const getMessages = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id
+    const { receiverId } = req.params
+
+    const messages = await Message.find({
+      $or: [ // find msg from both sides eg: sender: 111 reciver: 222 finds the messages and even if the sender becomes 222 and reciver 111 the msg is found
+        {
+          senderId: loggedInUserId,
+          receiverId: receiverId,
+        },
+        {
+          senderId: receiverId,
+          receiverId: loggedInUserId,
+        },
+      ],
+    }).sort({ createdAt: 1 })
+
+    res.status(200).json({
+      message: 'Messages fetched successfully',
+      messages,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Server error',
+    })
+  }
+}
+
+module.exports = {
+  sendMessage,
+  getMessages,
+}
